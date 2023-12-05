a5ed1e8f4ea8096e3bbfb53e0a52d414c3552685
Surprisebacon <isak.mathisen@hotmail.no>
2023-12-05 15:55:49 +0100

    Komemntar

--- jobbsokere/jobbsokerprofil/view_cv.php
+++ jobbsokere/jobbsokerprofil/view_cv.php
@@ -1,7 +1,6 @@
 <?php
 if (isset($_GET['cv_path'])) {
     $cvPath = $_GET['cv_path'];
-    // Add appropriate headers for PDF files
     header('Content-Type: application/pdf');
     readfile($cvPath);
 } else {
